81d53b23e9d56eabf870b49e0d57ce7d3b99440d
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-28 14:58:51 +0530

    28/09

---
+++ 10_addition.c
@@ -0,0 +1,12 @@
+//wap to add two  number take by user .
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter frist number = ");
+	scanf("%d",&a);
+	printf("enter second number = ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("sum of %d and %d = %d",a,b,c);
+}
