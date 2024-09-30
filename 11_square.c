68aaf2d3d6f9c6003a75146f9dc6cb6e543a8e44
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-30 14:52:16 +0530

    30/09

---
+++ 11_square.c
@@ -0,0 +1,11 @@
+//wap to print square of given number.
+#include<stdio.h>
+void main()
+{
+	int num,sqr;
+	printf("enter a num : ");
+	scanf("%d",&num);//5
+	sqr=num*num;
+	printf("square of %d = %d",num,sqr);
+	
+}
---
+++ 12_circle_area.c
@@ -0,0 +1,10 @@
+//wap to calculate area of circle.
+#include<stdio.h>
+void main()
+{
+	float radius,area;
+	printf("enter radius of circle : ");
+	scanf("%f",&radius);
+	area=3.141*radius*radius;
+	printf("area of circle = %.2f",area);
+}
