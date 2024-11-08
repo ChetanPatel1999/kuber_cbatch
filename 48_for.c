a13eed0255eb5e104c674fe67462faafb2be8713
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-11-08 11:54:19 +0530

    8/11

---
+++ 48_for.c
@@ -0,0 +1,13 @@
+//wap to print a msg 5 times.
+#include<stdio.h>
+void main()
+{
+ int i;
+ printf("before for loop\n");
+ for( i=1 ; i<=5  ;  i++ )//11
+ {
+    printf("hi students \n");//6
+    printf("ap kaise ho\n");
+ }
+ printf("after for loop");
+}
---
+++ 51_for.c
@@ -0,0 +1,10 @@
+// wap to print 100 to 1 number.
+#include <stdio.h>
+void main()
+{
+    int i;
+    for (i = 100; i >= 1; i--) //0
+    {
+        printf("%d ", i); // 10 9 8 7 6 5 4 3 2 1 
+    }
+}
